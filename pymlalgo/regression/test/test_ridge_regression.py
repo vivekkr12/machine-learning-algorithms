9d193888b5396157791df12c79f72f5453118f7a
Vivek Kumar <vivekuma@uw.edu>
2019-06-19 19:55:02 -0700

    convert ridge regression and ridge regression test to n cross d

--- pymlalgo/regression/test/test_ridge_regression.py
+++ pymlalgo/regression/test/test_ridge_regression.py
@@ -14,44 +14,44 @@ class RigdeRegressionTest(unittest.TestCase):
         cls.n, cls.d = 1000, 10
         np.random.seed(7)
 
-        x = np.random.uniform(0, 100, cls.n * cls.d).reshape(cls.d, cls.n)
-        eps = np.random.normal(loc=0, scale=5, size=cls.n * cls.d).reshape(cls.d, cls.n)
-        beta = np.random.uniform(1, 5, cls.d).reshape(cls.d, 1)
-        y = (x + eps).T.dot(beta)
+        x = np.random.uniform(0, 100, cls.n * cls.d).reshape(cls.n, cls.d)
+        eps = np.random.normal(loc=0, scale=5, size=cls.n * cls.d).reshape(cls.n, cls.d)
+        w = np.random.uniform(1, 5, cls.d).reshape(cls.d, 1)
+        y = (x + eps).dot(w)
 
         x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=7, test_size=0.25)
 
-        y_normalizer = Standardizer(x_train, Standardizer.FEATURES)
-        cls.x_train_std = y_normalizer.standardize(x_train)
-        cls.x_test_std = y_normalizer.standardize(x_test)
+        x_standardizer = Standardizer(x_train)
+        cls.x_train_std = x_standardizer.standardize(x_train)
+        cls.x_test_std = x_standardizer.standardize(x_test)
 
-        y_normalizer = Standardizer(y_train, Standardizer.LABELS)
-        cls.y_train_std = y_normalizer.standardize(y_train)
-        cls.y_test_std = y_normalizer.standardize(y_test)
+        y_standardizer = Standardizer(y_train)
+        cls.y_train_std = y_standardizer.standardize(y_train)
+        cls.y_test_std = y_standardizer.standardize(y_test)
 
-        cls.model = RidgeRegression(cls.x_train_std, cls.y_train_std, lambd=0.1 / cls.n, min_grad=0.00001)
+        cls.model = RidgeRegression(cls.x_train_std, cls.y_train_std, lambd=0.1 / cls.n)
         cls.model.train()
 
     def test_coefficients(self):
-        expected_coef = np.array([0.4590115, 0.28894745, 0.27342446, 0.21193167, 0.29020445,
-                                 0.26846985, 0.38304797, 0.12870089, 0.42763333, 0.0962281]).reshape(-1, 1)
-        coef = self.model.beta
-        np.testing.assert_array_almost_equal(expected_coef, coef, decimal=4)
+        expected_weights = np.array([0.49164553, 0.32841494, 0.29913547, 0.24813988, 0.31602083,
+                                     0.30158783, 0.41265551, 0.14537066, 0.4524806, 0.09626513]).reshape(-1, 1)
+        weights = self.model.w
+        np.testing.assert_array_almost_equal(expected_weights, weights, decimal=2)
 
     def test_final_cost(self):
-        expected_final_cost = 0.026745657020640256
+        expected_final_cost = 0.03355478376080152
         self.assertAlmostEqual(expected_final_cost, self.model.cost_history[-1], places=4)
 
     def test_predictions_shape(self):
         predictions = self.model.predict(self.x_test_std)
-        self.assertTupleEqual(predictions.shape, (self.x_test_std.shape[1], 1))
+        self.assertTupleEqual(predictions.shape, (self.x_test_std.shape[0], 1))
 
     def test_train_score(self):
-        expected_train_score = 0.9731618072817436
+        expected_train_score = 0.9665547873239119
         train_score = self.model.r_squared(self.x_train_std, self.y_train_std)
         self.assertAlmostEqual(expected_train_score, train_score, places=4)
 
     def test_test_score(self):
-        expected_test_score = 0.9773325023618751
+        expected_test_score = 0.9767454565709043
         test = self.model.r_squared(self.x_test_std, self.y_test_std)
         self.assertAlmostEqual(expected_test_score, test, places=4)
